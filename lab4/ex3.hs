904f157c4dd565cab6bd09442241c189e8f47aea
komilll <kamil9772@wp.pl>
2019-11-26 23:43:11 +0100

    lab4 con

--- lab4/ex3.hs
+++ lab4/ex3.hs
@@ -14,15 +14,18 @@ sumBinTree EmptyBT = 0
 sumBinTree (NodeBT n lt rt) = n + sumBinTree lt + sumBinTree rt
 
 data Expr a = Lit a |
-              Add (Expr a) (Expr a)
+              Add (Expr a) (Expr a) |
+              Substract (Expr a) (Expr a)
 
 eval :: Num a => Expr a -> a
 eval (Lit n) = n
 eval (Add e1 e2) = eval e1 + eval e2
+eval (Substract e1 e2) = eval e1 - eval e2
 
 show' :: Show a => Expr a -> String
 show' (Lit n) = show n
 show' (Add e1 e2) = "(" ++ show' e1 ++ "+" ++ show' e2 ++ ")"
+show' (Substract e1 e2) = "(" ++ show' e1 ++ "-" ++ show' e2 ++ ")"
 
 -- Exercices --
 --1
@@ -45,4 +48,41 @@ insert x (NodeBT n lt rt)
             | n < x = NodeBT n lt (insert x rt)
             | n > x = NodeBT n (insert x lt) rt
 
-list2BST :: Orda a => [a] -> BinTree a
+list2BST :: Ord a => [a] -> BinTree a
+list2BST [] = EmptyBT
+list2BST (x:xs) = list2BSTCon (NodeBT x EmptyBT EmptyBT) xs
+    where
+        list2BSTCon t [] = t
+        list2BSTCon t (x:xs) = list2BSTCon (insert x t) xs
+
+--2
+occurs :: Eq a => a -> BinTree a -> Int
+occurs _ EmptyBT = 0
+occurs val (NodeBT n lt rt) = occursCon val (NodeBT n lt rt) 0
+        where 
+            occursCon val EmptyBT acc = acc
+            occursCon val (NodeBT n lt rt) acc
+                                            | val == n = acc + 1 + occursCon val lt 0 + occursCon val rt 0
+                                            | otherwise = acc + occursCon val lt 0 + occursCon val rt 0
+
+elemOf :: Eq a => a -> BinTree a-> Bool
+elemOf _ EmptyBT = False
+elemOf val t = elemOfCon val t
+            where 
+                elemOfCon val EmptyBT = False
+                elemOfCon val (NodeBT n lt rt)
+                                            | n == val = True
+                                            | otherwise = (elemOfCon val lt) || (elemOfCon val rt)
+
+-- minElemOf :: Ord a => a -> BinTree a -> a
+-- minElemOf val EmptyBT = val
+-- minElemOf val (NodeBT n lt rt) =
+--     let leftMin = minElemOf val lt
+--         rightMin = minElemOf val rt
+--     in if (leftMin < rightMin)
+--         then
+--             leftMin
+--         else 
+--             rightMin
+
+--3
---
+++ lab4/ex5.hs
@@ -0,0 +1,31 @@
+newtype MyInt = MkMyInt Int
+
+instance Eq MyInt where
+    (==) (MkMyInt i1) (MkMyInt i2) = i1 == i2
+
+instance Ord MyInt where
+    (<=) (MkMyInt i1) (MkMyInt i2) = i1 <= i2
+
+instance Num MyInt where
+    (+) (MkMyInt i1) (MkMyInt i2) = MkMyInt (i1 + i2)
+    (-) (MkMyInt i1) (MkMyInt i2) = MkMyInt (i1 - i2)
+    (*) (MkMyInt i1) (MkMyInt i2) = MkMyInt (i1 * i2)
+    negate (MkMyInt i)            = MkMyInt (negate i)
+    abs (MkMyInt i)               = MkMyInt (abs i)
+    signum (MkMyInt i)            = MkMyInt (signum i)
+    fromInteger int               = MkMyInt (fromIntegral int)
+
+instance Show MyInt where
+    show (MkMyInt i) = "MkMyInt " ++ show i
+
+--Exercices
+--2
+data BinTree a = EmptyBT | 
+                NodeBT a (BinTree a) (BinTree a)
+                -- deriving (Eq, Ord, Show, Read)
+
+instance Eq a => Eq (BinTree a) where
+    (==) (EmptyBT) (EmptyBT) = True
+    (==) EmptyBT (NodeBT n2 lt2 rt2) = False
+    (==) (NodeBT n2 lt2 rt2) EmptyBT = False
+    (==) (NodeBT n1 lt1 rt1) (NodeBT n2 lt2 rt2) = n1 == n2 && lt1 == lt2 && rt1 == rt2
